f8bb13f4a366a0ff49ff40646535401a48b0eb16
Arthur G. <me@arthurka.eu>
2024-07-30 16:59:14 +0200

    WIP

--- commands/faucet.js
+++ commands/faucet.js
@@ -1,19 +1,25 @@
-const { amount, amountEth, infura, arbiscanUrl } = require('../config.json');
+const { amount, amountEth, infura, arbiscanUrl, movementExplorerUrl } = require('../config.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const sendViaAlchemy = require('../utils/sendViaAlchemy.js');
 const sendViaInfura = require('../utils/sendViaInfura.js');
+const getMoveHash = require('../utils/getMoveHash.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('faucet')
 		.setDescription('Request testnet funds from the faucet')
 		.addStringOption(option =>
+			option.setName('chain')
+				.setDescription('The chain to receive funds from ("arb" for Arbitrum Sepolia, "move" for Move Testnet)')
+				.setRequired(true))
+		.addStringOption(option =>
 			option.setName('address')
 				.setDescription('The address to request funds from the faucet')
 				.setRequired(true)),
 	async execute(interaction) {
 		const address = interaction.options.get('address').value.trim();
+		const chain = interaction.options.get('chain').value.trim();
 
 		const reply = infura ?
 			'Request sent to Infura. Please check the link to see if it\'s mined.'
@@ -21,21 +27,64 @@ module.exports = {
 			// TODO: Change this to "Please wait for it to be mined" once Alchemy Notify is set up.
 			'Request sent to Alchemy. Please check the link to see if it\'s mined.';
 
-		await	interaction.reply(reply);
+		await interaction.reply(reply);
 
-		const request = infura ? await sendViaInfura(address, amount, amountEth) : await sendViaAlchemy(address, amount);
+		try {
+			let request;
+			let tokenTransactionHash = '';
+			let ethTransactionHash = '';
+			let tokenExplorerUrl = '';
+			let ethExplorerUrl = '';
+			let ethSign = '';
 
-		if (request.status === 'success') {
-			const embed = new MessageEmbed()
-				.setColor('#3BA55C')
-				.setDescription(`[View on Arbiscan](${arbiscanUrl}${request.message})`);
-			const embedEth = new MessageEmbed()
-				.setColor('#3BA55C')
-				.setDescription(`[View on Arbiscan](${arbiscanUrl}${request.messageEth})`);
-			return interaction.followUp({ content: `Transaction for ${amount} OVL and ${amountEth} Eth created.`, embeds: [embed, embedEth] });
-		}
-		else {
-			return interaction.followUp(`Failed to send funds. Error: ${request.message}`);
+			if (chain === 'arb' || chain === 'move') {
+				// Send the transaction and get the response
+				request = await sendViaInfura(address, amount, amountEth, chain)
+
+				if (request.status === 'success') {
+					if (chain === 'move') {
+						tokenTransactionHash = await getMoveHash(request.message);
+						tokenExplorerUrl = `${movementExplorerUrl}${tokenTransactionHash}`;
+
+						ethTransactionHash = await getMoveHash(request.messageEth);
+						ethExplorerUrl = `${movementExplorerUrl}${ethTransactionHash}`;
+
+						ethSign = 'MOVE';
+					} else {
+						tokenTransactionHash = request.message;
+						tokenExplorerUrl = `${arbiscanUrl}${tokenTransactionHash}`;
+
+						ethTransactionHash = request.messageEth;
+						ethExplorerUrl = `${arbiscanUrl}${ethTransactionHash}`;
+
+						ethSign = 'ETH';
+					}
+				}
+			} else {
+				return interaction.followUp('Unsupported chain specified.');
+			}
+
+			if (request.status === 'success') {
+				const embed = new MessageEmbed()
+					.setColor('#3BA55C')
+					.setDescription(
+						`Token Transaction: [View on Explorer](${tokenExplorerUrl})`
+
+					);
+
+				const embedEth = new MessageEmbed()
+					.setColor('#3BA55C')
+					.setDescription(
+						`ETH Transaction: [View on Explorer](${ethExplorerUrl})`
+					);
+
+				return interaction.followUp({ content: `Transaction for ${amount} OVL and ${amountEth} ${ethSign} created.`, embeds: [embed, embedEth] });
+			} else {
+				return interaction.followUp(`Failed to send funds. Error: ${request.message}`);
+			}
+		} catch (error) {
+			console.error(error);
+			return interaction.followUp(`An error occurred: ${error.message}`);
 		}
 	},
 };
--- utils/erc20Contract.js
+++ utils/erc20Contract.js
@@ -1,18 +1,18 @@
-const { ALCHEMY_URL, INFURA_URL, FROM_ADDRESS, infura, NETWORK, tokenAddress } = require('../config.json');
 const ethers = require('ethers');
 
-module.exports = async (provider) => {
-	const abi = [
+module.exports = async (wallet, contractAddress) => {
+    const abi = [
         // Read-Only Functions
         "function balanceOf(address owner) view returns (uint256)",
         "function decimals() view returns (uint8)",
         "function symbol() view returns (string)",
-    
+
         // Authenticated Functions
         "function transfer(address to, uint amount) returns (bool)",
-    
+
         // Events
         "event Transfer(address indexed from, address indexed to, uint amount)"
     ];
-    return new ethers.Contract(tokenAddress, abi, provider)
-}
+
+    return new ethers.Contract(contractAddress, abi, wallet);
+};
