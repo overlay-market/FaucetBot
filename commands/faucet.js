138785cf89a2119a385ee1f0e467080f967c453a
Arthur G. <me@arthurka.eu>
2024-12-14 14:51:56 +0100

    feat: BERA only

--- commands/faucet.js
+++ commands/faucet.js
@@ -1,111 +1,57 @@
-const { amount, infura, arbiscanUrl, movementExplorerUrl, beraExplorerUrl, AMOUNT_ETH } = require('../config.json');
+const { amount, infura, beraExplorerUrl, AMOUNT_ETH } = require('../config.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const sendViaInfura = require('../utils/sendViaInfura.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('faucet')
-		.setDescription('Request testnet funds from the faucet')
-		.addStringOption(option =>
-			option.setName('chain')
-				.setDescription('The chain to receive funds from ("arb" Arbitrum Sepolia, "move" Move Testnet, "bera" Bartio Testnet)')
-				.setRequired(true))
-		.addStringOption(option =>
-			option.setName('address')
-				.setDescription('The address to request funds from the faucet')
-				.setRequired(true)),
-	async execute(interaction) {
-		const address = interaction.options.get('address').value.trim();
-		const chain = interaction.options.get('chain').value.trim().toLowerCase();
+    data: new SlashCommandBuilder()
+        .setName('faucet')
+        .setDescription('Request testnet funds from the faucet')
+        .addStringOption(option =>
+            option.setName('address')
+                .setDescription('The address to request funds from the faucet')
+                .setRequired(true)),
+    async execute(interaction) {
+        const address = interaction.options.get('address').value.trim();
+        const chain = 'bera';
 
-		const reply = infura ?
-			'Request sent. Please check the link to see if it\'s mined.'
-			:
-			// TODO: Change this to "Please wait for it to be mined" once Alchemy Notify is set up.
-			'Request sent to Alchemy. Please check the link to see if it\'s mined.';
+        const reply = infura ?
+            'Request sent. Please check the link to see if it\'s mined.'
+            :
+            'Request sent to Alchemy. Please check the link to see if it\'s mined.';
 
-		await interaction.reply(reply);
+        await interaction.reply(reply);
 
-		try {
-			let request;
-			let tokenExplorerUrl = '';
-			let ethExplorerUrl = '';
-			let ethSymbol = '';
-			let amountEth;
+        try {
+            const amountEth = AMOUNT_ETH.bera;
+            const request = await sendViaInfura(address, amount, amountEth, chain);
 
-			if (chain === 'arb' || chain === 'move' || chain === 'bera') {
-				switch (chain) {
-					case 'arb':
-						amountEth = AMOUNT_ETH.arb;
-						break;
-					case 'move':
-						amountEth = AMOUNT_ETH.move
-						break;
-					case 'bera':
-						amountEth = AMOUNT_ETH.bera
-						break;
-				}
-				// Send the transaction and get the response
-				request = await sendViaInfura(address, amount, amountEth, chain)
+            if (request.status === 'success') {
+                const tokenTransactionHash = request.message;
+                const ethTransactionHash = request.messageEth;
+                
+                const tokenExplorerUrl = `${beraExplorerUrl}${tokenTransactionHash}`;
+                const ethExplorerUrl = `${beraExplorerUrl}${ethTransactionHash}`;
 
-				const tokenTransactionHash = request.message;
-				const ethTransactionHash = request.messageEth;
+                const embed = new MessageEmbed()
+                    .setColor('#3BA55C')
+                    .setDescription(
+                        `Token Transaction: [View on Explorer](${tokenExplorerUrl})`
+                    );
 
-				if (request.status === 'success') {
-					if (chain === 'move') {
-						tokenExplorerUrl = `${movementExplorerUrl}${tokenTransactionHash}`;
-						ethExplorerUrl = `${movementExplorerUrl}${ethTransactionHash}`;
+                const embedEth = new MessageEmbed()
+                    .setColor('#3BA55C')
+                    .setDescription(
+                        `BERA Transaction: [View on Explorer](${ethExplorerUrl})`
+                    );
 
-						ethSymbol = 'MOVE';
-					} else if (chain === 'bera') {
-						tokenExplorerUrl = `${beraExplorerUrl}${tokenTransactionHash}`;
-						ethExplorerUrl = `${beraExplorerUrl}${ethTransactionHash}`;
-
-						ethSymbol = 'BERA';
-					} else {
-						tokenExplorerUrl = `${arbiscanUrl}${tokenTransactionHash}`;
-						ethExplorerUrl = `${arbiscanUrl}${ethTransactionHash}`;
-
-						ethSymbol = 'ETH';
-					}
-				}
-			} else {
-				throw new Error('Unsupported chain specified. Use `arb`, `move`, or `bera`.');
-			}
-
-			if (request.status === 'success') {
-				if (chain === 'move') {
-					const embed = new MessageEmbed()
-						.setColor('#3BA55C')
-						.setDescription(
-							`Transaction: [View on Explorer](${tokenExplorerUrl})`
-
-						);
-
-					return interaction.followUp({ content: `Transaction for ${amount} OVL and ${amountEth} ${ethSymbol} created.`, embeds: [embed] });
-				} else {
-					const embed = new MessageEmbed()
-						.setColor('#3BA55C')
-						.setDescription(
-							`Token Transaction: [View on Explorer](${tokenExplorerUrl})`
-
-						);
-
-					const embedEth = new MessageEmbed()
-						.setColor('#3BA55C')
-						.setDescription(
-							`${ethSymbol} Transaction: [View on Explorer](${ethExplorerUrl})`
-						);
-
-					return interaction.followUp({ content: `Transaction for ${amount} OVL and ${amountEth} ${ethSymbol} created.`, embeds: [embed, embedEth] });
-				}
-			} else {
-				throw new Error(`Failed to send funds. Error: ${request.message}`);
-			}
-		} catch (error) {
-			console.error(error);
-			throw new Error(`An error occurred: ${error.message}`);
-		}
-	},
+                return interaction.followUp({ content: `Transaction for ${amount} OVL and ${amountEth} BERA created.`, embeds: [embed, embedEth] });
+            } else {
+                throw new Error(`Failed to send funds. Error: ${request.message}`);
+            }
+        } catch (error) {
+            console.error(error);
+            throw new Error(`An error occurred: ${error.message}`);
+        }
+    },
 };
--- commands/ping.js
+++ commands/ping.js
@@ -1,61 +1,42 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { INFURA_URL, FROM_ADDRESS, infura, CONTRACT_ADDRESSES, MOVE_URL, PRIVATE_KEY, BARTIO_URL } = require('../config.json');
+const { BARTIO_URL, FROM_ADDRESS, infura, CONTRACT_ADDRESSES, PRIVATE_KEY } = require('../config.json');
 const ethers = require('ethers');
 const erc20Contract = require('../utils/erc20Contract.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!, configured provider, faucet balance and donation address.'),
-	async execute(interaction) {
-		const networks = {
-			arb: {
-				contractAddress: CONTRACT_ADDRESSES.arb,
-				provider: new ethers.JsonRpcProvider(INFURA_URL),
-				symbol: 'ETH'
-			},
-			move: {
-				contractAddress: CONTRACT_ADDRESSES.move,
-				provider: new ethers.JsonRpcProvider(MOVE_URL),
-				symbol: 'MOVE'
-			},
-			bera: {
-				contractAddress: CONTRACT_ADDRESSES.bera,
-				provider: new ethers.JsonRpcProvider(BARTIO_URL),
-				symbol: 'BERA'
-			},
-		};
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!, configured provider, faucet balance and donation address.'),
+    async execute(interaction) {
+        const response = 'Information was requested. Waiting for the response.'
 
-		const response = 'Information was requested. Waiting for the response.'
+        await interaction.reply(response);
 
-		interaction.reply(response)
+        try {
+            const provider = new ethers.JsonRpcProvider(BARTIO_URL);
+            const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+            const tokenContract = await erc20Contract(wallet, CONTRACT_ADDRESSES.bera);
+            const fromAddress = ethers.getAddress(FROM_ADDRESS);
 
-		let responseMessage = 'Pong! Provider: ';
-		responseMessage += infura ? 'Infura' : 'Alchemy';
-		responseMessage += '\n';
+            const rawBalance = await provider.getBalance(fromAddress);
+            const rawTokenBalance = await tokenContract.balanceOf(fromAddress);
+            
+            const balance = ethers.formatEther(rawBalance);
+            const tokenBalance = ethers.formatEther(rawTokenBalance);
 
-		for (const [chain, { provider, contractAddress, symbol }] of Object.entries(networks)) {
-			let balance, tokenBalance;
-			try {
-				const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-				const tokenContract = await erc20Contract(wallet, contractAddress);
-				const fromAddress = ethers.getAddress(FROM_ADDRESS);
-				const rawBalance = await provider.getBalance(fromAddress);
-				const rawTokenBalance = await tokenContract.balanceOf(fromAddress);
-				balance = ethers.formatEther(rawBalance);
-				tokenBalance = ethers.formatEther(rawTokenBalance);
+            const balanceShort = balance.toString().slice(0, balance.toString().indexOf('.') + 3);
+            const balanceTokenShort = tokenBalance.toString().slice(0, tokenBalance.toString().indexOf('.') + 3);
 
-				const balanceShort = balance.toString().slice(0, balance.toString().indexOf('.') + 3);
-				const balanceTokenShort = tokenBalance.toString().slice(0, tokenBalance.toString().indexOf('.') + 3);
+            let responseMessage = 'Pong! Provider: ';
+            responseMessage += infura ? 'Infura' : 'Alchemy';
+            responseMessage += '\n';
+            responseMessage += `Current balance on BERA: ${balanceShort} BERA. Current ERC20 balance: ${balanceTokenShort} OVL.\n`;
+            responseMessage += `Donate: ${FROM_ADDRESS}`;
 
-				responseMessage += `Current balance on ${chain.toUpperCase()}: ${balanceShort} ${symbol}. Current ERC20 balance: ${balanceTokenShort} OVL.\n`;
-			} catch (e) {
-				console.error(e);
-				responseMessage += `Error getting balance for ${chain.toUpperCase()}. Please check logs.\n`;
-			}
-		}
-
-		responseMessage += `Donate: ${FROM_ADDRESS}`;
-		return interaction.followUp({ content: responseMessage });
-	},
+            return interaction.followUp({ content: responseMessage });
+        } catch (error) {
+            console.error(error);
+            return interaction.followUp({ content: 'Error getting balance for BERA. Please check logs.' });
+        }
+    },
 };
